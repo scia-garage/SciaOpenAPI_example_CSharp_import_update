be825bc135e8e048477d5f4ec3321f44048488ab
Adam Pelikán <adam.pelikan@projekcnikancelar.cz>
2019-03-14 12:55:34 +0100

    use relative paths, include template esa

--- Program.cs
+++ Program.cs
@@ -12,31 +12,32 @@ namespace SciaOpenAPI_example_cs_import_update
     {
         static void Main(string[] args)
         {
+            string MyAppPath = AppDomain.CurrentDomain.BaseDirectory;
             SCIA.OpenAPI.Environment env = new SCIA.OpenAPI.Environment(@"c:\scia\git\sen\a\bin\release32", @".\temp");
             env.RunSCIAEngineer(EnvESA80.TEnvESAApp_ShowWindow.eEPShowWindowShow);
-            
-            SCIA.OpenAPI.EsaProject UpXmlProj = env.OpenProject(@"c:\SCIA\GIThub\SciaOpenAPI_example_CSS.mat\res\empty.with.mat.lib.esa");
-            env.UpdateProject(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeXML, @"c:\SCIA\GIThub\SciaOpenAPI_example_cs_import_update\res\xml\test.xml", UpXmlProj.ProjectID);
-            UpXmlProj.CloseProject(EnvESA80.TEnvESAApp_SaveChanges.eEPSaveChangesNo);
 
-            SCIA.OpenAPI.EsaProject UpEsaProj = env.OpenProject(@"c:\SCIA\GIThub\SciaOpenAPI_example_CSS.mat\res\empty.with.mat.lib.esa");
-            env.UpdateProject(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeESA, @"c:\SCIA\GIThub\SciaOpenAPI_example_cs_import_update\res\esa\test.esa", UpEsaProj.ProjectID);
+            SCIA.OpenAPI.EsaProject UpXmlProj = env.OpenProject(System.IO.Path.Combine(MyAppPath, @"..\..\..\res\template.esa"));
+            env.UpdateProject(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeXML, System.IO.Path.Combine(MyAppPath, @"..\..\..\res\xml\test.xml"), UpXmlProj.ProjectID);
+            UpXmlProj.CloseProject(EnvESA80.TEnvESAApp_SaveChanges.eEPSaveChangesNo);
+            
+            SCIA.OpenAPI.EsaProject UpEsaProj = env.OpenProject(System.IO.Path.Combine(MyAppPath, @"..\..\..\res\template.esa"));
+            env.UpdateProject(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeESA, System.IO.Path.Combine(MyAppPath, @"..\..\..\res\esa\test.esa"), UpEsaProj.ProjectID);
             UpEsaProj.CloseProject(EnvESA80.TEnvESAApp_SaveChanges.eEPSaveChangesNo);
 
-            SCIA.OpenAPI.EsaProject UpIfcProj = env.OpenProject(@"c:\SCIA\GIThub\SciaOpenAPI_example_CSS.mat\res\empty.with.mat.lib.esa");
-            env.UpdateProject(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeIFC, @"c:\SCIA\GIThub\SciaOpenAPI_example_cs_import_update\res\ifc\test.ifc", UpIfcProj.ProjectID);
+            SCIA.OpenAPI.EsaProject UpIfcProj = env.OpenProject(System.IO.Path.Combine(MyAppPath, @"..\..\..\res\template.esa"));
+            env.UpdateProject(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeIFC, System.IO.Path.Combine(MyAppPath, @"..\..\..\res\ifc\test.ifc"), UpIfcProj.ProjectID);
             UpIfcProj.CloseProject(EnvESA80.TEnvESAApp_SaveChanges.eEPSaveChangesNo);
             
-            SCIA.OpenAPI.EsaProject UpRevitProj = env.OpenProject(@"c:\SCIA\GIThub\SciaOpenAPI_example_CSS.mat\res\empty.with.mat.lib.esa");
-            env.UpdateProject(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeRevit, @"c:\SCIA\GIThub\SciaOpenAPI_example_cs_import_update\res\revit\test.r2s", UpRevitProj.ProjectID);
+            SCIA.OpenAPI.EsaProject UpRevitProj = env.OpenProject(System.IO.Path.Combine(MyAppPath, @"..\..\..\res\template.esa"));
+            env.UpdateProject(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeRevit, System.IO.Path.Combine(MyAppPath, @"..\..\..\res\revit\test.r2s"), UpRevitProj.ProjectID);
             UpRevitProj.CloseProject(EnvESA80.TEnvESAApp_SaveChanges.eEPSaveChangesNo);
             
-            SCIA.OpenAPI.EsaProject UpTeklaProj = env.OpenProject(@"c:\SCIA\GIThub\SciaOpenAPI_example_CSS.mat\res\empty.with.mat.lib.esa");
-            env.UpdateProject(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeTekla, @"c:\SCIA\GIThub\SciaOpenAPI_example_cs_import_update\res\tekla\test.t2s",UpTeklaProj.ProjectID);
+            SCIA.OpenAPI.EsaProject UpTeklaProj = env.OpenProject(System.IO.Path.Combine(MyAppPath, @"..\..\..\res\template.esa"));
+            env.UpdateProject(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeTekla, System.IO.Path.Combine(MyAppPath, @"..\..\..\res\tekla\test.t2s"),UpTeklaProj.ProjectID);
             UpTeklaProj.CloseProject(EnvESA80.TEnvESAApp_SaveChanges.eEPSaveChangesNo);
             
-            SCIA.OpenAPI.EsaProject UpXlsxProj = env.OpenProject(@"c:\SCIA\GIThub\SciaOpenAPI_example_CSS.mat\res\empty.with.mat.lib.esa");
-            env.UpdateProject(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeAEF, @"c:\SCIA\GIThub\SciaOpenAPI_example_cs_import_update\res\aef\test.xlsx", UpXlsxProj.ProjectID);
+            SCIA.OpenAPI.EsaProject UpXlsxProj = env.OpenProject(System.IO.Path.Combine(MyAppPath, @"..\..\..\res\template.esa"));
+            env.UpdateProject(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeAEF, System.IO.Path.Combine(MyAppPath, @"..\..\..\res\aef\test.xlsx"), UpXlsxProj.ProjectID);
             UpXlsxProj.CloseProject(EnvESA80.TEnvESAApp_SaveChanges.eEPSaveChangesNo);
 
             //==========================================================
@@ -47,15 +48,15 @@ namespace SciaOpenAPI_example_cs_import_update
             Guid newTeklaproject;
             Guid newAefproject;
 
-            EsaProject ImXmlpProj = env.ImportProjectFromFile(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeXML, @"C:\SCIA\GIThub\SciaOpenAPI_example_cs_import_update\res\xml\test.xml", out newXmlproject);
-            //ESA project cannot be "imported" into SEn...just opened //EsaProject ImpEsaProj = env.ImportProjectFromFile(@"C:\SCIA\GIThub\SciaOpenAPI_example_cs_import_update\res\esa\test.esa", out newEsaproject);
-            EsaProject ImpIfcProj = env.ImportProjectFromFile(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeIFC, @"C:\SCIA\GIThub\SciaOpenAPI_example_cs_import_update\res\ifc\test.ifc", out newIfcproject);
-            EsaProject ImpRevitProj = env.ImportProjectFromFile(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeRevit, @"C:\SCIA\GIThub\SciaOpenAPI_example_cs_import_update\res\revit\test.r2s", out newRevitproject);
-            EsaProject ImpTeklaProj = env.ImportProjectFromFile(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeTekla, @"C:\SCIA\GIThub\SciaOpenAPI_example_cs_import_update\res\tekla\test.t2s", out newTeklaproject);
-            EsaProject ImpAefProj = env.ImportProjectFromFile(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeAEF, @"C:\SCIA\GIThub\SciaOpenAPI_example_cs_import_update\res\aef\test.xlsx", out newAefproject);
+            EsaProject ImXmlpProj = env.ImportProjectFromFile(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeXML, System.IO.Path.Combine(MyAppPath, @"..\..\..\res\xml\test.xml"), out newXmlproject);
+            //ESA project cannot be "imported" into SEn...just opened //EsaProject ImpEsaProj = env.ImportProjectFromFile(System.IO.Path.Combine(MyAppPath, @"..\..\..\res\esa\test.esa"), out newEsaproject);
+            EsaProject ImpIfcProj = env.ImportProjectFromFile(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeIFC, System.IO.Path.Combine(MyAppPath, @"..\..\..\res\ifc\test.ifc"), out newIfcproject);
+            EsaProject ImpRevitProj = env.ImportProjectFromFile(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeRevit, System.IO.Path.Combine(MyAppPath, @"..\..\..\res\revit\test.r2s"), out newRevitproject);
+            EsaProject ImpTeklaProj = env.ImportProjectFromFile(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeTekla, System.IO.Path.Combine(MyAppPath, @"..\..\..\res\tekla\test.t2s"), out newTeklaproject);
+            EsaProject ImpAefProj = env.ImportProjectFromFile(SCIA.OpenAPI.Environment.TypeOfExtProject.TypeAEF, System.IO.Path.Combine(MyAppPath, @"..\..\..\res\aef\test.xlsx"), out newAefproject);
 
             env.CloseAllProjects(EnvESA80.TEnvESAApp_SaveChanges.eEPSaveChangesNo);
-
+            
         }
     }
 }
